804425579a6c35803012fcd6be9545d803503130
Mish <mishuct26@gmail.com>
2019-08-18 00:49:39 +0200

    Adds Header component file

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
+// import Header  from '/Header.css'
+// since it's not a component, you don't import it as above but rather as below
+import './Header.css'
+
+
 
 const Header = () => {
-    return <h1>Header</h1>
+    return <h1 className="Header">Header</h1>
 }
 
 export default Header;
