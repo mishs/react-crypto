ca3ddf2a5469bb4960cccf6a639ca043d7ffe11a
Mish <mishuct26@gmail.com>
2019-08-18 00:53:06 +0200

    Demonstrates inline style

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,10 +3,15 @@ import React from 'react'
 // since it's not a component, you don't import it as above but rather as below
 import './Header.css'
 
-
+// demonstration of inline styling, 
+   //key is in camelCase style convention as below
+   const containerStyle = {
+       //remember-  key is in camelCase
+        fontSize : '60px'
+}
 
 const Header = () => {
-    return <h1 className="Header">Header</h1>
+    return <h1 className="Header" style={containerStyle}>Header</h1>
 }
 
 export default Header;
