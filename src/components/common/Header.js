59238362ad4b89872ed6be665aec133800aa07fb
Mish <mishuct26@gmail.com>
2019-08-18 02:09:04 +0200

    Imports logo image and assign styling

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,16 +2,22 @@ import React from 'react'
 // import Header  from '/Header.css'
 // since it's not a component, you don't import it as above but rather as below
 import './Header.css'
+import logo from './logo.png'
 
 // demonstration of internal styling, 
    //key is in camelCase style convention as below
    const containerStyle = {
        //remember-  key is in camelCase
-        fontSize : '60px'
+        fontSize : '40px'
 }
 
 const Header = () => {
-    return <h1 className="Header" style={containerStyle} style={{marginTop : '10px'}}>Header</h1>
+    return (
+        <div className="Header">
+         <img src={logo} alt="logo" className="Header-logo"/>
+         {/* <h1 className="Header" style={containerStyle} style={{marginTop : '10px'}}>Header</h1> */}
+         </div>
+        )
 }
 
 export default Header;
