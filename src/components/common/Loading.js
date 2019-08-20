ec2ba5271f8edab8691ff9d7c0c1cc8b62a4955d
Mish <mishuct26@gmail.com>
2019-08-20 12:56:23 +0200

    Creates Loading functional component

---
+++ src/components/common/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// create loading component as functional component since it's only presentational
+
+const Loading = () => {
+    return <div className="Loading" />
+}
+
+export default Loading;
