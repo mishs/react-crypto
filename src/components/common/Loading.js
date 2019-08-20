2d4e2d6449452564c640f129b2d48dfdff2b563c
Mish <mishuct26@gmail.com>
2019-08-20 13:02:34 +0200

    Imports Loading CSS into Loading file

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,4 +1,6 @@
 import React from 'react'
+// when you import a filqe as whole you don't need from, just specify file name as below
+import './Loading.css'
 
 // create loading component as functional component since it's only presentational
 
