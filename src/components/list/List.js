f60f939867b3601c1b7bbd4fd61be96d3bda6fe8
Mish <mishuct26@gmail.com>
2019-08-23 00:07:35 +0200

    Mapped API data outside table-dollar class name to avoid sharing styling of $ sign

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -81,10 +81,14 @@ class List extends React.Component {
                              {currency.name}
                          </td>
                          <td>
-                             <span className="Table-dollar">$ {currency.price}</span>
+                             <span className="Table-dollar">$
+                             {currency.price} 
+                             </span>
                          </td>
                          <td>
-                             <span className="Table-dollar">$ {currency.marketCap}</span>
+                             <span className="Table-dollar">$
+                             {currency.marketCap}
+                             </span>
                          </td>
                          <td>
                              {this.renderChangePercent(currency.percentChange24h)}
