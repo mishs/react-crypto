d3130147b992d04c3999469ed0c16510da92c96e
Mish <mishuct26@gmail.com>
2019-08-23 00:58:01 +0200

    Adds properties to imported Table component

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
 import Loading from '../common/Loading';
-import './Table.css';
+import Table from './Table';
 
 
 class List extends React.Component {
@@ -62,41 +62,10 @@ class List extends React.Component {
         }
 
         return (
-            <div className="Table-container">
-                <table className="Table">
-                    <thead className="Table-head">
-                        <tr>
-                            <th>Cyptocurrency</th>
-                            <th>Price</th>
-                            <th>Market Cap</th>
-                            <th>24H Change</th>
-                        </tr> 
-                    </thead>
-                    <tbody className="Table-body">
-                     {currencies.map((currency) => 
-                        <tr key={currency.id}>
-                         <td>
-                             {/* this specific details, ie. table rank and name are from API. */}
-                             <span className="Table-rank">{currency.rank}</span>
-                             {currency.name}
-                         </td>
-                         <td>
-                             <span className="Table-dollar">$</span>
-                             {currency.price}
-                         </td>
-                         <td>
-                             <span className="Table-dollar">$</span>
-                             {currency.marketCap}
-                         </td>
-                         <td>
-                             {this.renderChangePercent(currency.percentChange24h)}
-                         </td>
-                        
-                        </tr>
-                     )}
-                    </tbody>
-                </table>
-            </div>
+            <Table 
+            currencies={currencies}
+            renderChangePercent={this.renderChangePercent}
+            />
         );
     }
 }
