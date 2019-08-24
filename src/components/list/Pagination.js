d08544e754327ff77bd0a973f582bdb7300e92e5
Mish <mishuct26@gmail.com>
2019-08-24 02:05:12 +0200

    Imports Pagination CSS

---
+++ src/components/list/Pagination.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Pagination.css'
+
+const Pagination = () => {
+ return <div className="Pagination">
+     <button className="Pagination-button">
+         &larr;
+     </button>
+
+     <button className="Pagination-button">
+         &rarr;
+     </button>
+ </div>
+}
+
+export default Pagination;
