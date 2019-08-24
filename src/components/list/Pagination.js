72c8eaff01745aab247b0aa38c3558398bd8ba3b
Mish <mishuct26@gmail.com>
2019-08-24 02:16:46 +0200

    Creates pagination span for pages and total Pages

--- src/components/list/Pagination.js
+++ src/components/list/Pagination.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Pagination.css'
 
-const Pagination = () => {
+const Pagination = (props) => {
+    const { page, totalPages } = props
  return <div className="Pagination">
      <button className="Pagination-button">
          &larr;
      </button>
+     <span className="Pagination-info">Page <b>{page}</b> of <b>{totalPages}</b></span>
 
      <button className="Pagination-button">
          &rarr;
