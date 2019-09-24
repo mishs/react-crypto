97b202a717f45d625769d28c4e890febeabb9ac3
Mish <mishuct26@gmail.com>
2019-09-24 22:33:49 +0200

    Creates NotFound Component

---
+++ src/components/notfound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import '/NotFound.css'
+
+// since it returns only some JSX, rather use Functional component
+const NoFound = () => {
+    return (
+        <div>
+            <h1>Oops! Page not Found</h1>
+        </div>
+    );
+}
+
+export default NoFound;
