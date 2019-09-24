945ac9707ad3bdd577a1518c530c2959ba25c6e9
Mish <mishuct26@gmail.com>
2019-09-24 22:04:53 +0200

    Imports BrowserRouter, Route and Switch components from DOM

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Header from './components/common/Header'
 import './index.css'
 import List from './components/list/List'
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      
-      <List />
-    </div>
+      <BrowserRouter>
+
+      </BrowserRouter>
   )
 }
 
