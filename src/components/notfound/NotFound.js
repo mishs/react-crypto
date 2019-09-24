7e3bad9d8ec01ed57fde69d8fdcb344830af42b0
Mish <mishuct26@gmail.com>
2019-09-24 22:48:17 +0200

    Imports CSS styling for NotFound page

--- src/components/notfound/NotFound.js
+++ src/components/notfound/NotFound.js
@@ -1,13 +1,13 @@
 import React from 'react'
-// import '/NotFound.css'
+import './NotFound.css'
 
 // since it returns only some JSX, rather use Functional component
-const NoFound = () => {
+const NotFound = () => {
     return (
-        <div>
+        <div className="NotFound">
             <h1>Oops! Page not Found</h1>
         </div>
     );
 }
 
-export default NoFound;
+export default NotFound;
